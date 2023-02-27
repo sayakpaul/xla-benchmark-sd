aef64c48c510705b364566731ba95660e395887f
Sayak Paul <spsayakpaul@gmail.com>
2023-02-27 15:32:56 +0530

    variable scoping.

--- benchmark.py
+++ benchmark.py
@@ -40,8 +40,25 @@ def load_concrete_fns_sd(jit_compile=True):
         decoder_fn = tf.function(decoder_fn, jit_compile=jit_compile)
         # Cannot XLA-compile the text encoder. See: https://github.com/tensorflow/tensorflow/issues/59818
 
-    # Variable bindings.
-    run_inference_concrete_fn(df_model_fn, text_encoder_fn, decoder_fn)
+    # Having to run these here instead of calling `run_inference_concrete_fn()`
+    # probably because of come variable-scoping problems.
+    # Diffusion model concrete func.
+    batch_size = tf.constant(NUM_IMAGES_TO_GEN)
+    context = tf.random.normal((batch_size, MAX_PROMPT_LENGTH, HIDDEN_DIM))
+    num_steps = tf.constant(10)
+    unconditional_guidance_scale = tf.constant(10.5)
+    _ = df_model_fn(
+        context=context,
+        num_steps=num_steps,
+        unconditional_context=context,
+        unconditional_guidance_scale=unconditional_guidance_scale,
+    )
+
+    # Text-encoder concrete func.
+    _ = text_encoder_fn(tokens=tf.ones((batch_size, MAX_PROMPT_LENGTH), tf.int32))
+
+    # Decoder concrete func.
+    _ = decoder_fn(latent=tf.random.normal((batch_size, LATENTS_RES, LATENTS_RES, 4)))
 
     return df_model_fn, text_encoder_fn, decoder_fn
 
